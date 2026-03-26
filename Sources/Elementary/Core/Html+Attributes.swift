e5c59d95a4e28c07bad31ed41c5e7e7c456b4dfe
Simon Leeb <52261246+sliemeobn@users.noreply.github.com>
2026-03-26 08:59:34 +0100

    stick to _attributes for now

--- Sources/Elementary/Core/Html+Attributes.swift
+++ Sources/Elementary/Core/Html+Attributes.swift
@@ -64,7 +64,7 @@ extension HTMLAttribute {
 }
 
 public protocol _Attributed {
-    var attributes: _AttributeStorage { get set }
+    var _attributes: _AttributeStorage { get set }
 }
 
 extension HTML where Self: _Attributed {
@@ -77,7 +77,7 @@ extension HTML where Self: _Attributed {
     public func attributes(_ attribute: HTMLAttribute<Tag>, when condition: Bool = true) -> Self {
         if condition {
             var element = self
-            element.attributes.append(_AttributeStorage(attribute))
+            element._attributes.append(_AttributeStorage(attribute))
             return element
         } else {
             return self
@@ -103,7 +103,7 @@ extension HTML where Self: _Attributed {
     public func attributes(contentsOf attributes: [HTMLAttribute<Tag>], when condition: Bool = true) -> Self {
         if condition {
             var element = self
-            element.attributes.append(_AttributeStorage(attributes))
+            element._attributes.append(_AttributeStorage(attributes))
             return element
         } else {
             return self
@@ -117,12 +117,18 @@ public struct _AttributedElement<Content: HTML>: HTML, _Attributed {
 
     public var content: Content
 
-    public var attributes: _AttributeStorage
+    @available(*, renamed: "_attributes")
+    public var attributes: _AttributeStorage {
+        _read { yield _attributes }
+        _modify { yield &_attributes }
+    }
+
+    public var _attributes: _AttributeStorage
 
     @usableFromInline
     init(content: Content, attributes: _AttributeStorage) {
         self.content = content
-        self.attributes = attributes
+        self._attributes = attributes
     }
 
     @inlinable
@@ -131,7 +137,7 @@ public struct _AttributedElement<Content: HTML>: HTML, _Attributed {
         into renderer: inout Renderer,
         with context: consuming _RenderingContext
     ) {
-        context.prependAttributes(html.attributes)
+        context.prependAttributes(html._attributes)
         Content._render(html.content, into: &renderer, with: context)
     }
 
@@ -142,7 +148,7 @@ public struct _AttributedElement<Content: HTML>: HTML, _Attributed {
         into renderer: inout Renderer,
         with context: consuming _RenderingContext
     ) async throws {
-        context.prependAttributes(html.attributes)
+        context.prependAttributes(html._attributes)
         try await Content._render(html.content, into: &renderer, with: context)
     }
 }
--- Sources/Elementary/Core/Html+Elements.swift
+++ Sources/Elementary/Core/Html+Elements.swift
@@ -5,13 +5,7 @@ public struct HTMLElement<Tag: HTMLTagDefinition, Content: HTML>: HTML, _Attribu
     public typealias Body = Never
     public typealias Content = Content
 
-    @available(*, deprecated, message: "`var _attributes` is deprecated, use `var attributes` instead")
-    public var _attributes: _AttributeStorage {
-        get { attributes }
-        set { attributes = newValue }
-    }
-
-    public var attributes: _AttributeStorage
+    public var _attributes: _AttributeStorage
 
     // The content of the element.
     public var content: Content
@@ -20,7 +14,7 @@ public struct HTMLElement<Tag: HTMLTagDefinition, Content: HTML>: HTML, _Attribu
     /// - Parameter content: The content of the element.
     @inlinable
     public init(@HTMLBuilder content: () -> Content) {
-        self.attributes = .init()
+        self._attributes = .init()
         self.content = content()
     }
 
@@ -30,7 +24,7 @@ public struct HTMLElement<Tag: HTMLTagDefinition, Content: HTML>: HTML, _Attribu
     ///   - content: The content of the element.
     @inlinable
     public init(_ attribute: HTMLAttribute<Tag>, @HTMLBuilder content: () -> Content) {
-        self.attributes = .init(attribute)
+        self._attributes = .init(attribute)
         self.content = content()
     }
 
@@ -40,7 +34,7 @@ public struct HTMLElement<Tag: HTMLTagDefinition, Content: HTML>: HTML, _Attribu
     ///  - content: The content of the element.
     @inlinable
     public init(_ attributes: HTMLAttribute<Tag>..., @HTMLBuilder content: () -> Content) {
-        self.attributes = .init(attributes)
+        self._attributes = .init(attributes)
         self.content = content()
     }
 
@@ -50,7 +44,7 @@ public struct HTMLElement<Tag: HTMLTagDefinition, Content: HTML>: HTML, _Attribu
     ///  - content: The content of the element.
     @inlinable
     public init(attributes: [HTMLAttribute<Tag>], @HTMLBuilder content: () -> Content) {
-        self.attributes = .init(attributes)
+        self._attributes = .init(attributes)
         self.content = content()
     }
 
@@ -60,9 +54,9 @@ public struct HTMLElement<Tag: HTMLTagDefinition, Content: HTML>: HTML, _Attribu
         into renderer: inout Renderer,
         with context: consuming _RenderingContext
     ) {
-        html.attributes.append(context.attributes)
+        html._attributes.append(context.attributes)
 
-        renderer.appendToken(.startTag(Tag.name, attributes: html.attributes.flattened(), isUnpaired: false, type: Tag.renderingType))
+        renderer.appendToken(.startTag(Tag.name, attributes: html._attributes.flattened(), isUnpaired: false, type: Tag.renderingType))
         Content._render(html.content, into: &renderer, with: .emptyContext)
         renderer.appendToken(.endTag(Tag.name, type: Tag.renderingType))
     }
@@ -74,10 +68,10 @@ public struct HTMLElement<Tag: HTMLTagDefinition, Content: HTML>: HTML, _Attribu
         into renderer: inout Renderer,
         with context: consuming _RenderingContext
     ) async throws {
-        html.attributes.append(context.attributes)
+        html._attributes.append(context.attributes)
 
         try await renderer.appendToken(
-            .startTag(Tag.name, attributes: html.attributes.flattened(), isUnpaired: false, type: Tag.renderingType)
+            .startTag(Tag.name, attributes: html._attributes.flattened(), isUnpaired: false, type: Tag.renderingType)
         )
         try await Content._render(html.content, into: &renderer, with: .emptyContext)
         try await renderer.appendToken(.endTag(Tag.name, type: Tag.renderingType))
@@ -89,39 +83,33 @@ public struct HTMLVoidElement<Tag: HTMLTagDefinition>: HTML, _Attributed where T
     /// The type of the HTML tag this element represents.
     public typealias Tag = Tag
 
-    @available(*, deprecated, message: "`var _attributes` is deprecated, use `var attributes` instead")
-    public var _attributes: _AttributeStorage {
-        get { attributes }
-        set { attributes = newValue }
-    }
-
-    public var attributes: _AttributeStorage
+    public var _attributes: _AttributeStorage
 
     /// Creates a new HTML void element.
     @inlinable
     public init() {
-        self.attributes = .init()
+        self._attributes = .init()
     }
 
     /// Creates a new HTML void element with the specified attribute.
     /// - Parameter attribute: The attribute to apply to the element.
     @inlinable
     public init(_ attribute: HTMLAttribute<Tag>) {
-        self.attributes = .init(attribute)
+        self._attributes = .init(attribute)
     }
 
     /// Creates a new HTML void element with the specified attributes.
     /// - Parameter attributes: The attributes to apply to the element.
     @inlinable
     public init(_ attributes: HTMLAttribute<Tag>...) {
-        self.attributes = .init(attributes)
+        self._attributes = .init(attributes)
     }
 
     /// Creates a new HTML void element with the specified attributes.
     /// - Parameter attributes: The attributes to apply to the element as an array.
     @inlinable
     public init(attributes: [HTMLAttribute<Tag>]) {
-        self.attributes = .init(attributes)
+        self._attributes = .init(attributes)
     }
 
     @inlinable
@@ -130,8 +118,8 @@ public struct HTMLVoidElement<Tag: HTMLTagDefinition>: HTML, _Attributed where T
         into renderer: inout Renderer,
         with context: consuming _RenderingContext
     ) {
-        html.attributes.append(context.attributes)
-        renderer.appendToken(.startTag(Tag.name, attributes: html.attributes.flattened(), isUnpaired: true, type: Tag.renderingType))
+        html._attributes.append(context.attributes)
+        renderer.appendToken(.startTag(Tag.name, attributes: html._attributes.flattened(), isUnpaired: true, type: Tag.renderingType))
     }
 
     @inlinable
@@ -141,9 +129,9 @@ public struct HTMLVoidElement<Tag: HTMLTagDefinition>: HTML, _Attributed where T
         into renderer: inout Renderer,
         with context: consuming _RenderingContext
     ) async throws {
-        html.attributes.append(context.attributes)
+        html._attributes.append(context.attributes)
         try await renderer.appendToken(
-            .startTag(Tag.name, attributes: html.attributes.flattened(), isUnpaired: true, type: Tag.renderingType)
+            .startTag(Tag.name, attributes: html._attributes.flattened(), isUnpaired: true, type: Tag.renderingType)
         )
     }
 }
